f34a2e7f901b4196c396987a798045f404bca18a
jiayuli1104 <156461908+jiayuli1104@users.noreply.github.com>
2025-11-05 13:52:29 -0600

    test1

--- CODE.py
+++ CODE.py
@@ -1,1 +1,31 @@
+FUNCTION sortBooks(bookList):
+    # bookList is a list of books
+    # each book has: shelfNumber, returnOrder
+
+    FOR i FROM 1 TO length(bookList) - 1:
+        keyBook = bookList[i]
+        j = i - 1
+
+        # Compare keyBook with books before it
+        WHILE j >= 0 AND compare(keyBook, bookList[j]) IS TRUE:
+            bookList[j + 1] = bookList[j]     # shift right
+            j = j - 1
+
+        bookList[j + 1] = keyBook             # insert keyBook into position
+
+    RETURN bookList
+
+
+FUNCTION compare(bookA, bookB):
+    # Return TRUE if bookA should come before bookB
+
+    IF bookA.shelfNumber < bookB.shelfNumber:
+        RETURN TRUE
+
+    ELSE IF bookA.shelfNumber == bookB.shelfNumber:
+        IF bookA.returnOrder < bookB.returnOrder:
+            RETURN TRUE
+
+    RETURN FALSE
+
 
